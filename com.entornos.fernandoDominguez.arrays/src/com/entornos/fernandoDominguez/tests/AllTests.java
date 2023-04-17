bbcb9bef37e6f7c9430a67e24ccbfe2443cfea37
Ferdomi21 <fernandodominguezlago@gmail.com>
2023-04-17 21:34:06 +0200

    Añadiendo test JUnite Suite Case

---
+++ com.entornos.fernandoDominguez.arrays/src/com/entornos/fernandoDominguez/tests/AllTests.java
@@ -0,0 +1,10 @@
+package com.entornos.fernandoDominguez.tests;
+
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+@Suite
+@SelectClasses({ TestCase.class })
+public class AllTests {
+
+}
