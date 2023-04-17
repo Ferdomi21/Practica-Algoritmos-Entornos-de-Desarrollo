97f6471414471518750a7ca44de9966cd74d14e5
Ferdomi21 <fernandodominguezlago@gmail.com>
2023-04-17 20:19:00 +0200

    Metodo media

--- com.entornos.fernandoDominguez.arrays/src/com/entornos/fernandoDominguez/arrays/MisArrays.java
+++ com.entornos.fernandoDominguez.arrays/src/com/entornos/fernandoDominguez/arrays/MisArrays.java
@@ -2,4 +2,23 @@ package com.entornos.fernandoDominguez.arrays;
 
 public abstract class MisArrays {
 
+	
+	
+	/**
+	 * Calcular media aritmética
+	 * @param array
+	 * @return double media
+	 */
+	public static double media(int array[]) {
+		int suma = 0;
+		for (int i = 0; i < array.length; i++) {
+			suma += array[i];
+		}
+
+		double media = (double) suma / array.length;
+		return media;
+	}
+
+	
+	
 }
